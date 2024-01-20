30242e918333caf5b6130ff154cfb765aeadeae6
Mike5719 <149823846+Mike5719@users.noreply.github.com>
2024-01-19 20:12:29 -0500

    models and seeding

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/connection');
 const { User, Blog } = require('../models');
 
 const userData = require('./userData.json');
-const BlogData = require('./projectData.json');
+const blogData = require('./blogData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
